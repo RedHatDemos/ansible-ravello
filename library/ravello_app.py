48cf6066d926ff1fc83d0793662c02e4a9d66258
Timothy Appnel <tima@ansible.com>
2016-08-18 12:18:48 -0400

    Refines ravello_app docs and adds examples

--- library/ravello_app.py
+++ library/ravello_app.py
@@ -24,57 +24,125 @@ options:
   name:
     description:
       - Unique application instance name
-    required: yes
+    required: true 
   description:
     description:
       - Application instance description that will be set on creation
+    required: false
   state:
     description:
       - Indicate desired state of the application instance.
     default: present
     choices: ['present', 'absent', 'started', 'stopped', 'restarted']
+    required: false
   username:
     description:
       - Ravello system username
-    required: yes
+    required: true
   password:
     description:
       - Ravello system password
-    required: yes
+    required: true
   blueprint:
     description:
-      - Create application instance based on an existing blueprint
-      - Is mututally exclusive with app_template, one of which is required for present or started where creation of a Ravello application may occur
+      - Create application instance based on an existing blueprint. See note below.
+    required: false
   app_template:
     description:
-      - Create application instance based on a app template.
-      - Is mututally exclusive with blueprint, one of which is required for present or started where creation of a Ravello application may occur.
+      - Create application instance based on a app template. See note below.
+    required: false
   preferred_cloud:
     description:
       - Preferred cloud provider to publish the Ravello application if creation is needed. 
   preferred_region:
     description:
       - Preferred region to publish the Ravello application if creation is needed.
+    required: false
   optimization_level:
     description:
       - TBD
     default: cost
+    required: false
     choices: ['cost', 'performance']
   application_ttl:
     description:
-     - Ravello application autostop defined in minutes
+      - Ravello application autostop defined in minutes
+    required: false
     default: -1
   wait:
     description:
-     - Wait for the Ravello application to be in state 'running' before returning.
+      - Wait for the Ravello application to be in state 'running' before returning.
     default: True
+    required: false
     choices: [ True, False ]
   wait_timeout:
     description:
-     - How long before wait gives up, in seconds.
+      - How long before wait gives up, in seconds.
+    required: false
     default: 600
-'''
+notes:
+  - C(app_template) and C(blueprint) are mututally exclusive. One of which is required when C(state) is present or started where creation of a Ravello application may need to occur.
+  - Publishing preferences, C(preferred_cloud), C(preferred_region) and C(optimization_level), are only considered when initially published. Using different values will not change the publishing state of the Ravello application.  
+  '''
 
 EXAMPLES = '''
+# Publish and launch application from an app template
+- ravello_app:
+    name: "unique-rav-app"
+    app_template: /path/to/ravello_app.template
+    username: admin
+    password: secret
+    state: started
+
+# Publish and launch application from an existing blueprint 
+- ravello_app:
+    name: "unique-rav-app"
+    blueprint: "existing-rav-app.bp"
+    username: admin
+    password: secret
+    state: started
+
+# Publish application with preferences and auto stop after 2 days
+- ravello_app:
+    name: "unique-rav-app"
+    blueprint: "existing-rav-app.bp"
+    username: admin
+    password: secret
+    preferred_region: Oregon
+    preferred_cloud: AMAZON
+    optimization_level: performance
+    application_ttl: 2880
+    state: started
+
+# QUESTION: WHAT IF STARTED THEN PRESENT CALLED? RESPOND OK AND NO CHANGES? ERROR?
+# Publish application but do not start VMs
+- ravello_app:
+    name: "unique-rav-app"
+    app_template: /path/to/ravello_app.template
+    username: admin
+    password: secret
+    state: present 
+
+# Stop application
+- ravello_app:
+    name: "unique-rav-app"
+    username: admin
+    password: secret
+    state: stopped 
+
+# Stop and delete application
+- ravello_app:
+    name: "unique-rav-app"
+    username: admin
+    password: secret
+    state: absent 
+
+# Restart application
+- ravello_app:
+    name: "unique-rav-app"
+    username: admin
+    password: secret
+    state: restarted
+
 '''
 
